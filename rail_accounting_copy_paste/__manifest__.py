09fef7f4339995f3e44e12f7fe8ac4f6317ccf34
APACIAP <103467266+APACIAP@users.noreply.github.com>
2023-04-18 08:37:10 -0600

    fix email error copypaste

--- rail_accounting_copy_paste/__manifest__.py
+++ rail_accounting_copy_paste/__manifest__.py
@@ -20,6 +20,7 @@
     # always loaded
     'data': [
         'data/cron.xml',
+        'data/sync_error_email_template.xml',
         'security/ir.model.access.csv',
         'security/security.xml',
         'views/copy_paste_template.xml',
