27fa9c74eefa67420b5c29d828b2d5ff567c2f61
KevinITGT <information.techgt@gmail.com>
2025-12-05 15:49:52 -0600

    Revert "comment local taxes retention"
    This reverts commit c41d906c98f9e8ff9908c4bcf6c36a74ca1dd422.

--- rail_accounting_copy_paste/models/account_move.py
+++ rail_accounting_copy_paste/models/account_move.py
@@ -3,16 +3,6 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import UserError, ValidationError
 
-class LocalTaxLines(models.Model):
-     _name = 'account.move.local.tax.lines'
-     _description = "Store the local tax deductions"
-
-     move_id = fields.Many2one('account.move')
-     tax_id = fields.Many2one('account.tax', string="Impuesto")
-     amount = fields.Float('Monto')
-
-
-
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
@@ -23,18 +13,42 @@ class AccountMove(models.Model):
     matrix_ref = fields.Char('Matrix ref', copy=False)
 
     #-----------
-    # LOCAL TAXES
+    # ISERTP WITHOLDING
     #-----------
 
-    local_tax_ids = fields.One2many(string="Impuestos locales")
+    use_isertp = fields.Boolean(string="Impuestos locales")
 
-    def create_write_local_taxes(self):
+    isertp_amount = fields.Float(string="ISERTP")
+    servicio_control = fields.Float(string="Servicios de control")
+    icic = fields.Float(string="I.C.I.C.")
+    cmicem = fields.Float(string="C.M.I.C.E.M")
+    impuestos_locales = fields.Float("Impuestos Locales")
+
+    def update_isertp(self):
         for r in self:
-
+            if r.use_isertp:
+                isertp_val = False
+                for l in r.line_ids:
+                    for t in l.tax_ids:
+                        if t.name == 'Impuestos Locales':
+                            isertp_val = True
+                if isertp_val:
+                    raise ValidationError("Ya estan establecidos los impuestos locales.\n"\
+                                        +"Antes de calcular el ISERTP debes remover dichos impuestos de todas las lineas implicadas")
+                else:
+                    isertp = self.env['account.tax'].search([('name','=','3.0% ISERTP'),('company_id','=',self.company_id.id)])
+                    if isertp:
+                        isertp.update({
+                            'amount': self.isertp_amount * -1,
+                        })
+                        local_tax = self.env['account.tax'].search([('name','=','Impuestos Locales'),('company_id','=',self.company_id.id)])
                         if local_tax:
                             for il in r.invoice_line_ids:
                                 il.write({
                                     'tax_ids': [(4, local_tax.id, 0)]
                                 })
-
+                    else:
+                        raise ValidationError("No se ha encontraro el impuesto relacionado a ISERTP \n"\
+                                            +"Verifica que haya un impuesto creado con el nombre '3.0% ISERTP'\n"\
+                                                + "Si no sabes donde buscarlo contacta a soporte.")
     
